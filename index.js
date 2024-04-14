8e7d9fb348702dfe60c41c44dbbd9ab67bf300fc
Ruth <57313608+ruth-cohen89@users.noreply.github.com>
2024-04-14 11:12:57 +0300

    ,oved index.js to async/await

--- index.js
+++ index.js
@@ -7,28 +7,12 @@ const entryPoint = "https://www.example.com";
 const folderPath = createLocalFolder(entryPoint);
 
 async function main() {
-  const entryPoint = "https://www.larstornoe.com";
-  const folderPath = createLocalFolder(entryPoint);
-
   try {
     const pagesDownloaded = await crawlWebsite(entryPoint, folderPath);
     console.log(`Pages downloaded: ${Array.from(pagesDownloaded).join(", ")}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
-
-  // try {
-  //   const pagesDownloaded = await crawlWebsite(entryPoint, folderPath);
-  //   console.log(`Pages downloaded: ${Array.from(pagesDownloaded).join(", ")}`);
-  // } catch (error) {
-  //   console.error(`Error: ${error.message}`);
-  // }
 }
 
 main();
-
-// crawlWebsite(entryPoint, folderPath)
-//   .then((pagesDownloaded) =>
-//     console.log(`Pages downloaded: ${Array.from(pagesDownloaded).join(", ")}`).then()
-//   )
-//   .catch((error) => console.error(`Error: ${error.message}`));
